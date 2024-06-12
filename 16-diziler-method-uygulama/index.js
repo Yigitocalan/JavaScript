1e0623e962f814bf719739c8d9f3a1165abbaa7e
yigit ocalan <ocalanyigit3@gmail.com>
2024-06-12 17:21:24 +0300

    app

---
+++ 16-diziler-method-uygulama/index.js
@@ -0,0 +1,9 @@
+let costomers= ["emir","asli","yigit"];
+let costomers1=["emin",["monster" , "dell","casper",[10,20,30]]];
+
+
+console.log(`urun: ${costomers1[1][0]} fiyati: ${costomers1[1][3][0]}`);
+
+console.log(costomers[0]);
+console.log(costomers[costomers.length -1]);
+console.log(costomers.length -1);
